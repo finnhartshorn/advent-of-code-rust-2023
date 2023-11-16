df040c7df6a3aaa7d0e6f78d364fb22573f8eb14
Finn Hartshorn <finnhartshorn@gmail.com>
2023-11-16 11:03:00 +1000

    Initial commit

---
+++ src/template/commands/scaffold.rs
@@ -0,0 +1,93 @@
+use std::{
+    fs::{File, OpenOptions},
+    io::Write,
+    process,
+};
+
+const MODULE_TEMPLATE: &str = r#"pub fn part_one(input: &str) -> Option<u32> {
+    None
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    None
+}
+
+advent_of_code::main!(DAY);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
+"#;
+
+fn safe_create_file(path: &str) -> Result<File, std::io::Error> {
+    OpenOptions::new().write(true).create_new(true).open(path)
+}
+
+fn create_file(path: &str) -> Result<File, std::io::Error> {
+    OpenOptions::new().write(true).create(true).open(path)
+}
+
+pub fn handle(day: u8) {
+    let day_padded = format!("{day:02}");
+
+    let input_path = format!("data/inputs/{day_padded}.txt");
+    let example_path = format!("data/examples/{day_padded}.txt");
+    let module_path = format!("src/bin/{day_padded}.rs");
+
+    let mut file = match safe_create_file(&module_path) {
+        Ok(file) => file,
+        Err(e) => {
+            eprintln!("Failed to create module file: {e}");
+            process::exit(1);
+        }
+    };
+
+    match file.write_all(MODULE_TEMPLATE.replace("DAY", &day.to_string()).as_bytes()) {
+        Ok(()) => {
+            println!("Created module file \"{}\"", &module_path);
+        }
+        Err(e) => {
+            eprintln!("Failed to write module contents: {e}");
+            process::exit(1);
+        }
+    }
+
+    match create_file(&input_path) {
+        Ok(_) => {
+            println!("Created empty input file \"{}\"", &input_path);
+        }
+        Err(e) => {
+            eprintln!("Failed to create input file: {e}");
+            process::exit(1);
+        }
+    }
+
+    match create_file(&example_path) {
+        Ok(_) => {
+            println!("Created empty example file \"{}\"", &example_path);
+        }
+        Err(e) => {
+            eprintln!("Failed to create example file: {e}");
+            process::exit(1);
+        }
+    }
+
+    println!("---");
+    println!(
+        "🎄 Type `cargo solve {}` to run your solution.",
+        &day_padded
+    );
+}
