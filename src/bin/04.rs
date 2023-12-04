25b0a2d9240d0a7a292778bb9c46de3622abfd14
Finn Hartshorn <finnhartshorn@gmail.com>
2023-12-05 08:14:20 +1100

    Formatting, minor fixes

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,38 +1,65 @@
 pub fn part_one(input: &str) -> Option<u32> {
-    Some(input.lines().map(
-        |line| {
-            let mut iter = line.split(':').nth(1).unwrap().split('|');
-            let win = iter.next().unwrap().split_whitespace().map(|x| x.parse::<u32>().unwrap()).collect::<Vec<u32>>();
-            let have = iter.next().unwrap().split_whitespace().map(|x| x.parse::<u32>().unwrap()).collect::<Vec<u32>>();
-            let points: u32 = have.iter().map(|x| if win.contains(x) { 1 } else { 0 }).sum();
-            if points == 0 {
-                return 0
-            }
-            2_i32.pow(points-1) as u32
-        }
-    ).sum())
+    Some(
+        input
+            .lines()
+            .map(|line| {
+                let mut iter = line.split(':').nth(1).unwrap().split('|').map(|x| {
+                    x.split_whitespace()
+                        .map(|x| x.parse::<u32>().unwrap())
+                        .collect::<Vec<u32>>()
+                });
+                let win = iter.next().unwrap();
+                let have = iter.next().unwrap();
+                let points: u32 = have
+                    .iter()
+                    .map(|x| if win.contains(x) { 1 } else { 0 })
+                    .sum();
+                if points == 0 {
+                    return 0;
+                }
+                2_i32.pow(points - 1) as u32
+            })
+            .sum(),
+    )
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
     let mut forward_vec = vec![1_u32; input.lines().count()];
-    Some(input.lines().map(
-        |line| {
-            let mut iter = line.split(':');
-            let card_num = iter.next().unwrap().split_whitespace().nth(1).unwrap().parse::<u32>().unwrap();
-            iter = iter.next().unwrap().split('|');
-            let win = iter.next().unwrap().split_whitespace().map(|x| x.parse::<u32>().unwrap()).collect::<Vec<u32>>();
-            let have = iter.next().unwrap().split_whitespace().map(|x| x.parse::<u32>().unwrap()).collect::<Vec<u32>>();
-            let points: u32 = have.iter().map(|x| if win.contains(x) { 1 } else { 0 }).sum();
-            let number_cards = forward_vec[card_num as usize - 1];
-            (0..points).for_each(|x| {
-                let card = card_num as usize + x as usize;
-                if card < forward_vec.len() {
-                    forward_vec[card] += number_cards;
-                }
-            });
-            number_cards
-        }
-    ).sum())
+    Some(
+        input
+            .lines()
+            .map(|line| {
+                let mut iter = line.split(':');
+                let card_num = iter
+                    .next()
+                    .unwrap()
+                    .split_whitespace()
+                    .nth(1)
+                    .unwrap()
+                    .parse::<u32>()
+                    .unwrap();
+                let mut iter2 = iter.next().unwrap().split('|').map(|x| {
+                    x.split_whitespace()
+                        .map(|x| x.parse::<u32>().unwrap())
+                        .collect::<Vec<u32>>()
+                });
+                let win = iter2.next().unwrap();
+                let have = iter2.next().unwrap();
+                let points: u32 = have
+                    .iter()
+                    .map(|x| if win.contains(x) { 1 } else { 0 })
+                    .sum();
+                let number_cards = forward_vec[card_num as usize - 1];
+                (0..points).for_each(|x| {
+                    let card = card_num as usize + x as usize;
+                    if card < forward_vec.len() {
+                        forward_vec[card] += number_cards;
+                    }
+                });
+                number_cards
+            })
+            .sum(),
+    )
 }
 
 advent_of_code::main!("04");
